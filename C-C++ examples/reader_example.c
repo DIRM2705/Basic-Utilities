278e7f66660f7c63d1a5cacabfa0c0a89b381eac
DIRM2705 <dirm2705@gmail.com>
2024-12-16 15:45:29 -0600

    Delete unnecesary files

--- C-C++ examples/reader_example.c
+++
@@ -1,8 +0,0 @@
-#include "pro_reader.h"
-
-void main() {
-    int i = read_int("Enter an integer: ");
-    float f = read_float("Enter a float: ");
-    char *s = read_string("Enter a string: ");
-    short b = read_bool("Enter a bool: ");
-}
