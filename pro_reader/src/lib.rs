da1006e2f1bbf4fab638dc22e677b48583608e8b
DIRM2705 <dirm2705@gmail.com>
2024-12-16 12:54:50 -0600

    Add pro_reader library

--- pro_reader/src/lib.rs
+++ pro_reader/src/lib.rs
@@ -1,14 +1,66 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::io::{stdin, Write, stdout};
+use std::ffi::CStr;
+
+#[no_mangle]
+pub extern "C" fn read_int(instruction : *const u8) -> i32 {
+    loop {
+        let input = read_input(instruction);
+        match input.parse() {
+            Ok(num) => return num,
+            Err(_) => {
+                println!("Ingrese un número válido");
+            }
+        }
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[no_mangle]
+pub extern "C" fn read_float(instruction : *const u8) -> f32 {
+    loop {
+        let input = read_input(instruction);
+        match input.parse() {
+            Ok(num) => return num,
+            Err(_) => {
+                println!("Ingrese un número válido");
+            }
+        }
     }
+}
+
+#[no_mangle]
+pub extern "C" fn read_bool(instruction : *const u8) -> bool {
+    loop {
+        let input = read_input(instruction);
+        match input.parse() {
+            Ok(num) => return num,
+            Err(_) => {
+                println!("Ingrese true o false");
+            }
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn read_string(instruction : *const u8) -> *const u8 {
+    return read_input(instruction).as_ptr() as *const u8;
+}
+
+fn read_input(instruction : *const u8) -> String {
+    //Flush stdout
+    stdout().flush().unwrap();
+
+    // Print the instruction
+    let c_str: &CStr = unsafe { CStr::from_ptr(instruction as *const i8) };
+    let str_slice: &str = c_str.to_str().unwrap();
+    println!("{}", str_slice);
+
+    // Read the input
+    let mut input = String::new();
+    match stdin().read_line(&mut input) {
+        Ok(_) => input = input.trim().to_string(),
+        Err(err) => println!("{err}")
+        
+    }
+
+    return input
 }
